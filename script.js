4bc2ca8b67a11788fd2af76a05ea0d2b826c0a31
Nick <korbofos.nn@gmail.com>
2019-05-28 00:33:08 +0300

    Это первый коммит

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+
+console.log(4/0)
+console.log('string'*9)
+let something;
+console.log(something)
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bnp'];
+
+console.log(arr[0])
+
+//alert ("Hello World!");
+
+//let answer = confirm("are you here?");
+
+//console.log(answer); 
+
+
+//let answer = +prompt("Усть ли вам 18?", "Да");
+
+//console.log(typeof(answer)); 
+
+//console.log("arr" + " - object")
+
+//console.log(4 + + " - object")
+
+let incr = 10,
+    decr = 10;
+
+ //incr ++;
+ //decr --;   
+
+ console.log(incr++);
+ console.log(decr--);
+
+ console.log(5%2);
+ console.log("2" === 2);
+
+ let isChecked = false, 
+     isClose = false;
+
+     console.log(isChecked || !isClose);
